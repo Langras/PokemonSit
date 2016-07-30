3ba1a4de5a66ac5bdf639b8d0d8b6f81826362a0
Oleksandr Dubenko <odybenko@gmail.com>
2016-07-30 13:05:54 +0300

    port

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var bodyParser = require('body-parser')
 var fs = require('fs');
 var jsonfile = require('jsonfile');
 var bots = [];
-var io = require('socket.io').listen(app.listen(process.env.PORT || 3000));
+var port = process.env.PORT || 3000
+var io = require('socket.io').listen(app.listen(port));
 var socket;
 var pokemonArray = require('./web/pokemondata.json');
 var logs = [];
@@ -23,7 +24,7 @@ app.use('/',express.static(__dirname + '/view'));
 
 
 console.log("# Pokemon Sit v0.1 \n");
-console.log("# App running at localhost:"+process.env.PORT || 3000);
+console.log("# App running at localhost:"+port);
 loadPSConfig();
 
 function loadPSConfig(){
